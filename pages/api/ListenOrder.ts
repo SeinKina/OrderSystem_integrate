ab5f5a0badc7fff6924e997f799bbe1019e1bd8f
仲村 優佳 <ynakamura704@gmail.com>
2024-10-22 08:49:54 +0900

    注文除法表示pushに変更

--- pages/api/ListenOrder.ts
+++ pages/api/ListenOrder.ts
@@ -90,13 +90,13 @@ export async function listenOrder(event: line.WebhookEvent, client: line.messagi
                     for (const [storeId, orders] of Object.entries(storeOrderMap)) {
                         const flexMsg = await flexMessage(orders);
                         const waitTimeValue = Math.floor(userOrderData.waitTime.get(storeId) / 60000);  // ミリ秒を分に変換
-                        await client.replyMessage({
-                            replyToken: event.replyToken,
+                        await client.pushMessage({
+                            to: userId,
                             messages: [flexMsg,{type:"text", "text":`${orders[0].storeName}の注文です\n待ち時間は約${waitTimeValue} 分です`},],
                         });
                     }
-                    await client.replyMessage({
-                        replyToken: event.replyToken,
+                    await client.pushMessage({
+                        to: userId,
                         messages: [{type:"text", "text": `${userOrderData.clientName}さんの注文は以上です\n屋台ごとに調理が完了でき次第お呼びします！しばらくお待ちください\n\n※待ち時間は大幅に前後する可能性があります`},],
                     });
                 }
