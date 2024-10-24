d46b3cd5f186b470b8781d9f0db6cd319e154f67
仲村 優佳 <ynakamura704@gmail.com>
2024-10-24 11:36:39 +0900

    chengeStream変更してテストのためのコミット

--- pages/api/listener/ListenUpdateOrderData.js
+++ pages/api/listener/ListenUpdateOrderData.js
@@ -26,25 +26,43 @@ export default async function ListenUpdateOrderData(req, res) {
         StoreOrder = mongoose.model(collectionName, StoreOrderSchema);
       }
 
-      // StoreOrder の変更を監視
-      const storeOrderChangeStream = StoreOrder.watch();
+      let resumeToken = null;
 
-      storeOrderChangeStream.on('change', async (change) => {
-        // cookStatus が true に変更されたときの処理
-        if (change.operationType === 'update' && change.updateDescription.updatedFields.cookStatus === true) {
-          console.log('cookStatus が true に更新されました。');
-          const updatedOrder = await StoreOrder.findById(change.documentKey._id);
-          const userOrderData =await getUpdateOrderData(updatedOrder); 
-          if (userOrderData.success === false){
-            console.log("できなかったっぽ:" , userOrderData.message);
-            return
+      createChangeStream = () => {
+        const options = resumeToken ? { resumeAfter: resumeToken } : {};
+        const storeOrderChangeStream = StoreOrder.watch([], options);
+        storeOrderChangeStream.on('change', async (change) => {
+          resumeToken = change._id ? change._id : change._resumeToken;
+
+          // cookStatus が true に変更されたときの処理
+          if (change.operationType === 'update' && change.updateDescription.updatedFields.cookStatus === true) {
+            console.log('cookStatus が true に更新されました。');
+            const updatedOrder = await StoreOrder.findById(change.documentKey._id);
+            const userOrderData =await getUpdateOrderData(updatedOrder); 
+            if (userOrderData.success === false){
+              console.log("できなかったっぽ:" , userOrderData.message);
+              return
+            }
+            console.log("引数:", userOrderData);
+            SendCookedOrders(userOrderData.orderDetails);
           }
-          console.log("引数:", userOrderData);
-          SendCookedOrders(userOrderData.orderDetails);
-          // 必要な処理をここに追加
-        }
+        });
+
+        // エラーハンドリングと再接続
+        storeOrderChangeStream.on('error', (error) => {
+          console.error('changestreamエラー:', error);
+          setTimeout(createChangeStream, 5000);
+        });
+
+        storeOrderChangeStream.on('end', () => {
+          console.error('changestreamが終了しました。再接続するよ');
+          setTimeout(createChangeStream, 5000);
+        });
+      };
+
+      createChangeStream();
+
     });
-  })
     res.status(200).json({ message: '変更の監視を開始しました' });
   } catch (error) {
     console.error('エラーが発生しました:', error);
